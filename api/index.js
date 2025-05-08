a7033e084c0e3884d15807753c721e7fce0c54e2
Abhinav Verma <>
2025-05-08 08:30:33 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,7 +13,6 @@ const expformidable = require("express-formidable");
 app.use(expformidable());
 // connect with MongoDB server
 
-
 async function connectDB() {
   const client = await mongodb.MongoClient.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
@@ -26,70 +25,67 @@ async function connectDB() {
 app.post("/upload", async function (request, result) {
   try {
     await connectDB();
-  // get input name="file" from client side
-  const file = request.files.file;
+    // get input name="file" from client side
+    const file = request.files.file;
 
-  // set file path in MongoDB GriDFS
-  // this will be saved as "filename" in "fs.files" collection
-  const filePath = file.name;
-  
-  const mm = await import('music-metadata');
-  const metadata = await mm.parseFile(file.path);
+    // set file path in MongoDB GriDFS
+    // this will be saved as "filename" in "fs.files" collection
+    const filePath = file.name;
 
-  if (metadata.common.picture && metadata.common.picture.length > 0) {
-    const image = metadata.common.picture[0]; // typically image/jpeg or image/png
+    const mm = await import("music-metadata");
+    const metadata = await mm.parseFile(file.path);
 
-    const uploadImage = imgBucket.openUploadStream(filePath, {
-      chunkSizeBytes: 1048576,
-      metadata: {
-        type: 'image',
-        linkedSong: filePath,
-        type: image.format
-      }
-    });
+    if (metadata.common.picture && metadata.common.picture.length > 0) {
+      const image = metadata.common.picture[0]; // typically image/jpeg or image/png
 
-    uploadImage.end(image.data);
-  }
-
-  // read user uploaded file stream
-  fs.createReadStream(file.path)
-    // add GridFS bucket stream to the pipe
-    // it will keep reading and saving file
-    .pipe(
-      bucket.openUploadStream(filePath, {
-        // maximum size for each chunk (in bytes)
-        chunkSizeBytes: 1048576, // 1048576 = 1 MB
-        // metadata of the file
+      const uploadImage = imgBucket.openUploadStream(filePath, {
+        chunkSizeBytes: 1048576,
         metadata: {
-          name: file.name, // file name
-          size: file.size, // file size (in bytes)
-          type: file.type, // type of file
+          type: "image",
+          linkedSong: filePath,
+          type: image.format,
         },
-      })
-    )
-    // this callback will be called when the file is done saving
-    .on("finish", function () {
-      result.send({ msg: "File saved." });
-    });
+      });
+
+      uploadImage.end(image.data);
+    }
+
+    // read user uploaded file stream
+    fs.createReadStream(file.path)
+      // add GridFS bucket stream to the pipe
+      // it will keep reading and saving file
+      .pipe(
+        bucket.openUploadStream(filePath, {
+          // maximum size for each chunk (in bytes)
+          chunkSizeBytes: 1048576, // 1048576 = 1 MB
+          // metadata of the file
+          metadata: {
+            name: file.name, // file name
+            size: file.size, // file size (in bytes)
+            type: file.type, // type of file
+          },
+        })
+      )
+      // this callback will be called when the file is done saving
+      .on("finish", function () {
+        result.send({ msg: "File saved." });
+      });
   } catch (error) {
     console.log(error);
     result.status(500).send({ msg: "Error uploading file." });
-    
   }
 });
 
-
-
-// app.get("/", async function (request, result) {
-//   await connectDB();
-//   // get all files from GridFS bucket
-//   const files = await bucket.find({}).toArray();
-//   const imgFiles = await imgBucket.find({}).toArray();
-//   result.render("index", {
-//     files,
-//     imgFiles,
-//   });
-// });
+app.get("/", async function (request, result) {
+  await connectDB();
+  // get all files from GridFS bucket
+  const files = await bucket.find({}).toArray();
+  const imgFiles = await imgBucket.find({}).toArray();
+  result.render("index", {
+    files,
+    imgFiles,
+  });
+});
 
 app.get("/songs", async function (request, result) {
   await connectDB();
@@ -129,7 +125,7 @@ app.get("/songs/:filename", async function (request, result) {
   // get file from GridFS bucket
   const files = await bucket
     .find({
-      filename
+      filename,
     })
     .toArray();
 
@@ -153,8 +149,7 @@ app.get("/songs/:filename", async function (request, result) {
 
   // bucket.openDownloadStreamByName(filename).pipe(result);
 
-  const readstream =  bucket.openDownloadStream(song._id);
-
+  const readstream = bucket.openDownloadStream(song._id);
 
   // for local file system
   // const readstream = fs.createReadStream(image.filename);
@@ -176,7 +171,7 @@ app.get("/images/:filename", async function (request, result) {
   // get file from GridFS bucket
   const files = await imgBucket
     .find({
-      filename
+      filename,
     })
     .toArray();
 
@@ -194,8 +189,7 @@ app.get("/images/:filename", async function (request, result) {
 
   // bucket.openDownloadStreamByName(filename).pipe(result);
 
-  const readstream =  imgBucket.openDownloadStream(image._id);
-
+  const readstream = imgBucket.openDownloadStream(image._id);
 
   // for local file system
   // const readstream = fs.createReadStream(image.filename);
@@ -209,7 +203,7 @@ app.get("/images/:filename", async function (request, result) {
   readstream.pipe(result);
 });
 
-app.listen(process.env.PORT || 5000, async ()=> {
+app.listen(process.env.PORT || 5000, async () => {
   console.log("Server started");
 });
 module.exports = app;
